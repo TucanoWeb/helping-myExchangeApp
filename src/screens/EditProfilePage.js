62f100fc93fbb7463eedf1cb54a9a590279f322d
vagne <vagnerpiresnh@hotmail.com>
2024-03-21 13:47:51 +0000

    EditProfile Update

--- src/screens/EditProfilePage.js
+++ src/screens/EditProfilePage.js
@@ -1,40 +1,70 @@
-import React, { useState } from 'react';
-import { TextInput, TouchableOpacity, Text, Platform, KeyboardAvoidingView, ScrollView } from 'react-native';
-import { ArrowLeftIcon } from 'react-native-heroicons/solid';
+import React, { useState, useEffect } from 'react';
+import { TextInput, TouchableOpacity, Text, Platform, KeyboardAvoidingView, ScrollView, Modal, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { doc, updateDoc } from "firebase/firestore";
-import { db } from '../config/firebase';
-import { launchImageLibrary } from 'react-native-image-picker';
+
+import { ArrowLeftIcon } from 'react-native-heroicons/solid';
+
 import DatePicker from '@react-native-community/datetimepicker';
+
+import useAuth from '../hooks/useAuth';
+
+import { collection, getDoc } from 'firebase/firestore';
+import { doc, setDoc, updateDoc } from "firebase/firestore";
+import { db } from '../config/firebase';
+
+import * as ImagePicker from 'expo-image-picker';
 
 const EditProfilePage = () => {
     const navigation = useNavigation();
+    const {user} = useAuth();
 
-    const [name, setName] = useState('');
-    const [birthdate, setBirthdate] = useState(new Date());
-    const [destinationCity, setDestinationCity] = useState('');
-    const [dateArrival, setDateArrival] = useState('');
-    const [originCity, setOriginCity] = useState('');
-    const [whatsapp, setWhatsapp] = useState('');
-    const [school, setSchool] = useState('');
-    const [airlines, setAirlines] = useState('');
-    const [photo, setPhoto] = useState('');
+    const [showSchoolPicker, setShowSchoolPicker] = useState(false);
+    const [selectedSchool, setSelectedSchool] = useState('');
     const [showDatePicker, setShowDatePicker] = useState(false);
     const [showDatePickerArrival, setShowDatePickerArrival] = useState(false);
 
+    const [dataForm, setDataForm] = useState({
+        name: "",
+        birthdate: new Date(),
+        destinationCity: "",
+        dateArrival: "",
+        originCity: "",
+        whatsapp: "",
+        school: [],
+        airlines: "",
+        photo: "",
+    })
+
     const saveProfile = async () => {
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+
         try {
-            await updateDoc(doc(db, "users", "yourUserId"), {
-                Name: name,
-                Birthdate: birthdate.toISOString(),
-                Destination_city: destinationCity,
-                Date_arrival: dateArrival,
-                Origin_city: originCity,
-                Whatsapp: `${whatsapp}`,
-                School: school,
-                Airlines: airlines,
-                Photo: photo,
-            });
+            if(docSnap.exists()) {
+                await updateDoc(docRef, {
+                    Name: dataForm.name,
+                    Birthdate: dataForm.birthdate.toISOString(),
+                    Destination_city: dataForm.destinationCity,
+                    Date_arrival: dataForm.dateArrival,
+                    Origin_city: dataForm.originCity,
+                    Whatsapp: `${dataForm.whatsapp}`,
+                    School: dataForm.school,
+                    Airlines: dataForm.airlines,
+                    Photo: dataForm.photo,
+                })
+            } else {
+                await setDoc(docRef, {
+                    Name: dataForm.name,
+                    Birthdate: dataForm.birthdate.toISOString(),
+                    Destination_city: dataForm.destinationCity,
+                    Date_arrival: dataForm.dateArrival,
+                    Origin_city: dataForm.originCity,
+                    Whatsapp: `${dataForm.whatsapp}`,
+                    School: dataForm.school,
+                    Airlines: dataForm.airlines,
+                    Photo: dataForm.photo,
+                })
+            }
             console.log('Profile saved!');
             navigation.navigate('Home');
         } catch (error) {
@@ -42,103 +72,170 @@ const EditProfilePage = () => {
         }
     };
 
-    const handleChoosePhoto = () => {
-        launchImageLibrary({ noData: true }, response => {
-            if (response.assets) {
-                setPhoto(response.assets[0].uri);
-            }
-        });
-    };
+    function onChangeForm(key, value) {
+        setDataForm(prev => ({
+          ...prev,
+          [key]: value
+        }))
+        console.log(dataForm)
+      }
+
+    const handleChoosePhoto = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+          });
+      
+          console.log(result);
+      
+          if (!result.canceled) {
+            onChangeForm("photo", result.assets[0].uri);
+          }
+        }
 
     const handleDateChange = (event, selectedDate) => {
         setShowDatePicker(Platform.OS === 'ios');
-        setBirthdate(selectedDate || birthdate);
+        onChangeForm("birthdate", selectedDate || dataForm.birthdate);
     };
 
     const handleDateArrivalChange = (event, selectedDate) => {
         setShowDatePickerArrival(Platform.OS === 'ios');
-        setDateArrival(selectedDate || dateArrival);
+        onChangeForm("dateArrival", selectedDate || dataForm.dateArrival);
+    };
+
+    // useEffect(() => {
+    //     const fetchSchools = async () => {
+    //         const querySnapshot = await getDocs(collection(db, "schools"));
+    //         const schoolsArray = querySnapshot.docs.map(doc => ({ id: doc.id, name: doc.data().School }));
+    //         setSchool(schoolsArray);
+    //     };
+    
+    //     fetchSchools();
+    // }, []);
+
+    const handleSelectSchool = (schoolName) => {
+        setSelectedSchool(schoolName);
+        setShowSchoolPicker(false);
     };
 
     return (
         <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}>
-        <ScrollView contentContainerStyle={styles.contentContainer}>
-            <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.buttonBack}>
-                <ArrowLeftIcon size="25" color="black" />
-            </TouchableOpacity>
-            <TextInput
-                style={styles.input}
-                placeholder="Name"
-                value={name}
-                onChangeText={setName}
-            />
-            <TouchableOpacity style={[styles.input, styles.datePickerInput]} onPress={() => setShowDatePicker(true)}>
-                <Text style={[styles.dateText, { color: birthdate instanceof Date && birthdate.toDateString() !== new Date().toDateString() ? 'black' : '#A9A9A9' }]}>
-                        {birthdate instanceof Date && birthdate.toDateString() !== new Date().toDateString() ? birthdate.toDateString() : 'Birthdate'}
-                </Text>
-            </TouchableOpacity>
-            {showDatePicker && (
-                <DatePicker
-                    value={birthdate}
-                    mode="date"
-                    display={Platform.OS === 'ios' ? 'spinner' : 'default'}
-                    onChange={handleDateChange}
+            style={{ flex: 1 }}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}>
+            
+            <ScrollView contentContainerStyle={styles.contentContainer}>
+            
+                <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.buttonBack}>
+                    <ArrowLeftIcon size="25" color="black" />
+                </TouchableOpacity>
+            
+                <TextInput
+                    style={styles.input}
+                    placeholder="Name"
+                    value={dataForm.name}
+                    onChangeText={e => onChangeForm("name", e)}
                 />
-            )}
-            <TextInput
-                style={styles.input}
-                placeholder="Destination City"
-                value={destinationCity}
-                onChangeText={setDestinationCity}
-            />
-            <TouchableOpacity style={[styles.input, styles.datePickerInput]} onPress={() => setShowDatePickerArrival(true)}>
-                <Text style={[styles.dateText, { color: dateArrival ? 'black' : '#A9A9A9' }]}>
-                    {dateArrival ? dateArrival.toDateString() : 'Date Arrival'}
-                </Text>
-            </TouchableOpacity>
-            {showDatePickerArrival && (
-                <DatePicker
-                    value={dateArrival || new Date()}
-                    mode="date"
-                    display={Platform.OS === 'ios' ? 'spinner' : 'default'}
-                    onChange={handleDateArrivalChange}
+            
+                <TouchableOpacity 
+                style={[styles.input, styles.datePickerInput]} 
+                onPress={() => setShowDatePicker(true)}
+                    >
+                    
+                    <Text style={[styles.dateText, { color: dataForm.birthdate instanceof Date && dataForm.birthdate.toDateString() !== new Date().toDateString() ? 'black' : '#A9A9A9' }]}
+                        >
+                        
+                        {dataForm.birthdate instanceof Date && dataForm.birthdate.toDateString() !== new Date().toDateString() ? dataForm.birthdate.toDateString() : 'Birthdate'}
+                    </Text>
+                </TouchableOpacity>
+
+                {showDatePicker && (
+                    <DatePicker
+                        value={dataForm.birthdate}
+                        mode="date"
+                        display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                        onChange={handleDateChange}
+                    />
+                )}
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Destination City"
+                    value={dataForm.destinationCity}
+                    onChangeText={e => onChangeForm("destinationCity", e)}
                 />
-            )}
-            <TextInput
-                style={styles.input}
-                placeholder="Origin City"
-                value={originCity}
-                onChangeText={setOriginCity}
-            />
-            <TextInput
-                style={styles.whatsappInput}
-                placeholder="WhatsApp number"
-                value={whatsapp}
-                onChangeText={setWhatsapp}
-                keyboardType="phone-pad"
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="School"
-                value={school}
-                onChangeText={setSchool}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Airlines"
-                value={airlines}
-                onChangeText={setAirlines}
-            />
-            <TouchableOpacity style={styles.uploadButton} onPress={handleChoosePhoto}>
-            <Text style={styles.uploadButtonText}>{photo ? 'Photo Selected' : 'Upload your photo'}</Text>
-        </TouchableOpacity>
-            <TouchableOpacity onPress={saveProfile} style={styles.button}>
-                <Text style={styles.buttonText}>SAVE</Text>
-            </TouchableOpacity>
-        </ScrollView>
+
+                <TouchableOpacity 
+                style={[styles.input, styles.datePickerInput]} 
+                onPress={() => setShowDatePickerArrival(true)}
+                    >
+                    <Text style={[styles.dateText, { color: dataForm.dateArrival ? 'black' : '#A9A9A9' }]}>
+                        {dataForm.dateArrival  ? dataForm.dateArrival.toDateString() : 'Date Arrival'}
+                    </Text>
+                </TouchableOpacity>
+
+                {showDatePickerArrival && (
+                    <DatePicker
+                        value={dataForm.dateArrival || new Date()}
+                        mode="date"
+                        display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                        onChange={handleDateArrivalChange}
+                    />
+                )}
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Origin City"
+                    value={dataForm.originCity}
+                    onChangeText={e => onChangeForm("originCity", e)}
+                />
+
+                <TextInput
+                    style={styles.whatsappInput}
+                    placeholder="WhatsApp number"
+                    value={dataForm.whatsapp}
+                    onChangeText={e => onChangeForm("whatsapp", e)}
+                    keyboardType="phone-pad"
+                />
+
+                <TouchableOpacity onPress={() => setShowSchoolPicker(true)} style={styles.input}>
+                    <Text style={[styles.dateText2, { color: selectedSchool ? 'black' : '#A9A9A9' }]}>
+                        {selectedSchool || "School"}
+                    </Text>
+                </TouchableOpacity>
+                <Modal
+                    visible={showSchoolPicker}
+                    animationType="slide"
+                    onRequestClose={() => setShowSchoolPicker(false)}>
+                    <FlatList
+                        data={dataForm.school}
+                        keyExtractor={item => item.id}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity onPress={() => handleSelectSchool(item.name)}>
+                                <Text>{item.name}</Text>
+                            </TouchableOpacity>
+                        )}
+                    />
+                </Modal>
+
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Airlines"
+                    value={dataForm.airlines}
+                    onChangeText={e => onChangeForm("airlines", e)}
+                />
+
+                <TouchableOpacity style={styles.uploadButton} onPress={handleChoosePhoto}>
+                    <Text style={styles.uploadButtonText}>{dataForm.photo ? 'Photo Selected' : 'Upload your photo'}</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={saveProfile} style={styles.button}>
+                    <Text style={styles.buttonText}>SAVE</Text>
+                </TouchableOpacity>
+            </ScrollView>
         </KeyboardAvoidingView>
     );
 };
@@ -191,6 +288,15 @@ const styles = {
         marginTop: 15, // Add space between inputs
     },
     dateText: {
+        fontSize: 15,
+        paddingLeft: 0,
+        justifyContent: 'center'
+    },
+    dateText2: {
+        fontSize: 15,
+        paddingLeft: 0,
+        justifyContent: 'center',
+        marginTop: 8
     },
     phoneSection: {
         flexDirection: 'row',
